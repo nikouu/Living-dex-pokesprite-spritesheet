ad56c3f4c42148d49434937e4ac4a404ac2325d4
Niko Uusitalo <983351+nikouu@users.noreply.github.com>
2022-08-08 17:47:37 +1200

    Renamed project

---
+++ PokespriteGenerator/Npm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokespriteGenerator
+{
+    internal class Npm
+    {
+        private readonly Uri NpmUrl = new("https://registry.npmjs.com/");
+
+        private void GetTarball(string packageName, string packageVersion)
+        {
+
+        }
+    }
+
+
+    internal class PackageMetadata
+    {
+        public DistributionTags disttags { get; set; }
+    }
+
+    internal class DistributionTags
+    {
+        public string latest { get; set; }
+    }
+
+    public class PackageVersionMetadata
+    {
+        public Distribution dist { get; set; }
+
+    }
+
+    public class Distribution
+    {
+        public string integrity { get; set; }
+        public string shasum { get; set; }
+        public string tarball { get; set; }
+        public int fileCount { get; set; }
+        public int unpackedSize { get; set; }
+        public string npmsignature { get; set; }
+        public Signature[] signatures { get; set; }
+    }
+
+    public class Signature
+    {
+        public string keyid { get; set; }
+        public string sig { get; set; }
+    }
+}
--- PokespriteGenerator/SpriteManipulator.cs
+++ PokespriteGenerator/SpriteManipulator.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SpritesheetGenerator
+namespace PokespriteGenerator
 {
-    internal class SpritesheetGenerator
+    internal class SpriteManipulator
     {
     }
 }
